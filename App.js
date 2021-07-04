a1b0a9d78fff62496aca0b66feae1bdcc84bee16
Alben Kalambukadu <albenk234@gmail.com>
2021-07-04 17:44:13 -0400

    Added ListingDetailsScreen and ListItem components.

--- App.js
+++ App.js
@@ -2,20 +2,12 @@ import React from 'react';
 import { View } from 'react-native';
 
 import Card from './app/components/Card';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 const App = () => {
   return (
-   <View style={{
-     backgroundColor: '#f8f4f4',
-     padding: 20,
-     paddingTop: 100,
-  }}>
-    <Card 
-      title="Red jacket for sale"
-      subTitle="$100"
-      image={require("./app/assets/jacket.jpg")}
-    />
-    </View>
+   
+    <ListingDetailsScreen />
   );
 }
 
