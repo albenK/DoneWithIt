91c59b30b415a27e3d435c5ac441443d3416ca9d
Alben Kalambukadu <albenk234@gmail.com>
2021-07-09 22:28:03 -0400

    Added AppOptionPicker component

--- App.js
+++ App.js
@@ -8,10 +8,12 @@ import ListingsScreen from './app/screens/ListingsScreen';
 import AppIcon from './app/components/AppIcon';
 import ListItem from './app/components/ListItem';
 import AppTextInput from './app/components/AppTextInput';
+import AppOptionPicker from './app/components/AppOptionPicker';
 
 const App = () => {
   return (
     <AppScreen>
+      <AppOptionPicker icon="apps" placeholder="Category"/>
       <AppTextInput icon="email" placeholder="Username"/>
     </AppScreen>
   );
---
+++ app/components/AppOptionPicker.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { Button, Modal, Dimensions, TouchableWithoutFeedback, View, StyleSheet } from 'react-native';
+
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import AppText from './AppText';
+import AppScreen from './AppScreen';
+import DEFAULT_STYLES from '../config/styles';
+
+const AppOptionPicker = ({ icon, placeholder }) => {
+    const [ isModalVisible, setIsModalVisible] = useState(false);
+
+    return (
+        <>
+
+            <TouchableWithoutFeedback onPress={() => setIsModalVisible(true)}>
+                <View style={styles.pickerButton}>
+                    {!!icon && <MaterialCommunityIcons
+                        name={icon}
+                        size={20}
+                        color={DEFAULT_STYLES.colors.medium}
+                        style={styles.icon}
+                    />}
+                    <AppText style={styles.text}>{placeholder}</AppText>
+                    <MaterialCommunityIcons
+                        name="chevron-down"
+                        size={20}
+                        color={DEFAULT_STYLES.colors.medium}
+                    />
+                </View>
+            </TouchableWithoutFeedback>
+
+            <Modal visible={isModalVisible} transparent={true} animationType="slide">
+                <View style={styles.pickerContainer}>
+                    <AppScreen>
+                        <Button title="CLOSE" onPress={() => setIsModalVisible(false)}/>
+                    </AppScreen>
+                </View>
+            </Modal>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    pickerContainer: {
+        width: '100%',
+        height: '100%',
+        paddingHorizontal: 15,
+        paddingBottom: 15,
+        backgroundColor: DEFAULT_STYLES.colors.white,
+    },
+    pickerButton: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: '100%',
+        padding: 15,
+        borderRadius: 25,
+        marginVertical: 10,
+        backgroundColor: DEFAULT_STYLES.colors.light,
+    },
+    icon: {
+        marginRight: 10,
+    },
+    text: {
+        flex: 1,
+    }
+});
+
+export default AppOptionPicker;
